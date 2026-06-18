8a3e6b516b66c054134e28eac2e1b7aece30159d
indrajithas673 <2003indrajithas@gmail.com>
2026-06-18 11:29:19 +0530

    chore(notification): scaffold project structure, models and configuration

---
+++ notification-service/src/main/java/com/ibatulanand/notificationservice/config/KafkaConfig.java
@@ -0,0 +1,19 @@
+package com.ibatulanand.notificationservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+@Configuration
+public class KafkaConfig {
+
+    @Bean
+    public TaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(5);
+        scheduler.setThreadNamePrefix("kafka-retry-");
+        scheduler.initialize();
+        return scheduler;
+    }
+}
