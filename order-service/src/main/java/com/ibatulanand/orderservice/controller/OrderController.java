5520c917fa8c15ff364181c60b4a05f3c3ccce41
indrajithas673 <2003indrajithas@gmail.com>
2026-07-01 17:00:03 +0530

    feat(order): expose REST endpoints and handle exceptions

---
+++ order-service/src/main/java/com/ibatulanand/orderservice/controller/OrderController.java
@@ -0,0 +1,33 @@
+package com.ibatulanand.orderservice.controller;
+
+import com.ibatulanand.orderservice.dto.OrderRequest;
+import com.ibatulanand.orderservice.dto.OrderResponse;
+import com.ibatulanand.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+@RestController
+@RequestMapping("/api/order")
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<OrderResponse> getAllOrders() {
+        return orderService.getAllOrders();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public CompletableFuture<String> placeOrder(@RequestBody OrderRequest orderRequest) {
+        return CompletableFuture.supplyAsync(() -> orderService.placeOrder(orderRequest));
+    }
+}
