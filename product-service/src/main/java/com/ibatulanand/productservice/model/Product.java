8fbc2bc117fc5a2e0c06e505df41d678f4cb8163
indrajithas673 <2003indrajithas@gmail.com>
2026-08-01 22:51:00 +0530

    feat: build module components for ProductServiceApplication.java

---
+++ product-service/src/main/java/com/ibatulanand/productservice/model/Product.java
@@ -0,0 +1,34 @@
+package com.ibatulanand.productservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = "products")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class Product {
+    @Id
+    private String id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+
+    @PrePersist
+    void assignId() {
+        if (id == null || id.isBlank()) {
+            id = UUID.randomUUID().toString();
+        }
+    }
+}
